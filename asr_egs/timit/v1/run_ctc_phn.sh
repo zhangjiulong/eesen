6816de096a1d5f220acaf261395f642f0b246d35
zhangjiulong <xljiulong@gmail.com>
2016-06-28 09:47:01 +0800

    decode version of eesen

--- asr_egs/timit/v1/run_ctc_phn.sh
+++ asr_egs/timit/v1/run_ctc_phn.sh
@@ -90,8 +90,8 @@ if [ $stage -le 3 ]; then
   echo "                            Decoding                               "
   echo =====================================================================
   # decoding
-#  steps/decode_ctc_lat.sh --cmd "$decode_cmd" --nj 8 --beam 17.0 --lattice_beam 8.0 --max-active 5000 --acwt 0.6 \
- #   data/lang_phn_test data/test $dir/decode_dev || exit 33;
+  # steps/decode_ctc_lat.sh --cmd "$decode_cmd" --nj 8 --beam 17.0 --lattice_beam 8.0 --max-active 5000 --acwt 0.6 \
+  # data/lang_phn_test data/test $dir/decode_dev || exit 33;
   steps/decode_ctc_lat.sh --cmd "$decode_cmd" --nj 11 --beam 17.0 --lattice_beam 8.0 --max-active 5000 --acwt 0.6 \
     data/lang_phn_test data/test $dir/decode_test || exit 34;
 fi
