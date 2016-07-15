f1dc7e1626e813fa9eafd8a69d341e6a3bb8bd6e
zhangjiulong <xljiulong@gmail.com>
2016-07-15 17:58:24 +0800

    bug

--- bin/install.sh
+++ bin/install.sh
@@ -15,4 +15,4 @@ sudo mount 192.168.100.35:/asrDataCenter $mountDir
 
 # modify auto mount script to fstab
 sudo cp /etc/fstab /etc/fstab.bak
-sudo sed -i '$a 192.168.100.35:/asrDataCenter    $mountDir   nfs ro,vers=3,rsize=8192,wsize=8192,acdirmin=5,acdirmax=8,hard,proto=tcp 0 0' /etc/fstab
+sudo sed -i '$a 192.168.100.35:/asrDataCenter    '$mountDir'   nfs ro,vers=3,rsize=8192,wsize=8192,acdirmin=5,acdirmax=8,hard,proto=tcp 0 0' /etc/fstab
